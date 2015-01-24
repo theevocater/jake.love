6a760e7de3233857e48925615074824e6709e5aa
Jake Kaufman <theevocater@gmail.com>
2015-01-23 20:25:24 -0800

    Add bump!

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.window.width = 16*64
   t.window.height = 8*64
-  local lurker,lume = 'modules/lurker/?.lua','modules/lume/?.lua'
-  package.path = package.path .. ';' .. lurker .. ';' .. lume
+  local lurker,lume,bump = 'modules/lurker/?.lua','modules/lume/?.lua','modules/bump/?.lua'
+  package.path = package.path .. ';' .. lurker .. ';' .. lume .. ';' .. bump
 end
